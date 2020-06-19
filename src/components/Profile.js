c976be5e736ac4e8cde5860e05d724697a3d8a92
fionna <fionnatong@gmail.com>
2020-06-19 23:34:28 +0800

    feat: add links

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,10 +12,37 @@ const Profile = () => (
       </h1>
     </div>
     <div className="profile__description">
-      <p>i'm a software engineer in sunny singapore.</p>
+      <p>i'm a budding software engineer in sunny singapore.</p>
       <p>
         i've been discovering the wonders of web development since graduation.
+        what's even better is that i get to use these skills to develop
+        solutions that bring meaning to citizens' lives!
       </p>
+      <p>
+        join me in my humble journey as i seek to learn from people who have
+        walked the path i wish to embark on and those who find meaning in what
+        they do :)
+      </p>
+    </div>
+    <div className="profile__link-segment">
+      <div className="profile__link">
+        <a
+          href="https://www.linkedin.com/in/fionnatong/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedin
+        </a>
+      </div>
+      <div className="profile__link">
+        <a
+          href="https://github.com/fionnatong"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </div>
     </div>
   </div>
 );
